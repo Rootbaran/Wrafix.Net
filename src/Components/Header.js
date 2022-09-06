39a391e57eab608838284886c43bd2f4f78e1690
baransoft <softbaran@yandex.com>
2022-09-07 00:25:53 +0300

    Demo ~V0.2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,8 @@ function Header() {
                 <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Projeler</a>
             </li>
             <li>
-                <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Referanslar</a>
+
+            <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
             </li>
         </ul>
     </div>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -11,13 +11,13 @@ function Hero() {
           <h2 className='flex font-medium'>
             sürekli geliştiriyoruz, geliştirmeye çalışıyoruz. <br/> topluluğa yararlı olmak için her geçen gün <br /> kendimizi daha da geliştiriyoruz.
           </h2>
-          <a href='#discover' className='inline-flex mt-5 uppercase bg-[#10ac84] text-white w-40 h-10 rounded-sm items-center justify-center hover:bg-[#10ce9e] transition-colors font-normal'> keşfe başla </a>
+          <a href='#discover' className='inline-flex mt-5 uppercase bg-[#0e57b8] text-white w-40 h-10 rounded-sm items-center justify-center hover:bg-[#1f4db1] transition-colors font-normal'> keşfe başla </a>
         </div>
         <div >
           <img 
           id='image-code'
           className='flex ml-auto mr-[220px]'
-          src='https://img.freepik.com/free-vector/open-source-concept-illustration_114360-3583.jpg?w=900&t=st=1662320844~exp=1662321444~hmac=da456f878cf1fd4c6acce0c55fecdd6052e0b7abc20e82457dff968704be7c8e' width='350'/>
+          src='https://img.freepik.com/free-vector/pair-programming-concept-illustration_114360-2170.jpg?w=740&t=st=1662499056~exp=1662499656~hmac=dca34ae4353eb10f12543216e336003f23b4b0392d9ed9bd334dfcdfd8ba6f5d' width='315'/>
         </div>
         <h2 id='linear' className='text-center mt-20 border-b-2 h-5 border-[#c2c2c2]'><h2 className='bg-white w-14 text-center mx-auto'>Wrafix</h2></h2>
     </div>

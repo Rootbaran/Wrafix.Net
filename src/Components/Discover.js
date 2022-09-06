26e0bd577695f7c1a9d3cb740e834b616551e4b6
baransoft <softbaran@yandex.com>
2022-09-06 17:46:00 +0300

    Demo ~V0.2

--- src/Components/Discover.js
+++ src/Components/Discover.js
@@ -6,7 +6,7 @@ function Discover() {
     <div>
         <div id='discover'>
             <h1 className='text-center font-bold text-2xl mt-10'>Wrafix'i Keşfedin</h1>
-            <h2 className='text-center font-medium text-xl mt-3'>Sizler için bir karşılık beklemeden <br /> 
+            <h2 className='text-center font-medium text-xl mt-3'>Sizler için kâr amacı gütmeden <br /> 
             web applikasyonları, yazılım otomasyonları, telegram botları ve daha nicelerini geliştiriyoruz.
             </h2> 
             <br />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,13 +8,13 @@ function Header() {
     <div>
         <ul className='bg-white h-16 text-black shadow-md'>
             <li className=''>
-                <a href="/" className='inline-flex font-semibold text-2xl uppercase font-[Montserrat] m-4 float-left '>wrafix</a>
+                <a href="/" className='inline-flex font-semibold text-2xl uppercase font-[Montserrat] m-4 float-left '>Wrafix.Net</a>
             </li>
             <li>
-                <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Deneme</a>
+                <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Projeler</a>
             </li>
             <li>
-                <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Deneme</a>
+                <a href='#' className='float-right m-[18px] text-xl uppercase font-thin font-sans'>Referanslar</a>
             </li>
         </ul>
     </div>

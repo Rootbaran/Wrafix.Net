f1d5a3742a57980548556114b84f747eaf419401
baransoft <softbaran@yandex.com>
2022-09-07 18:54:30 +0300

    Demo ~V0.3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,10 +8,16 @@ function Header() {
     <div class='h-15 border-b border-gray-100 w-full flex justify-between items-center px-4'>
        <ul className='bg-white h-16 text-black shadow-md'>
           <a href="/" className='inline-flex font-semibold text-2xl uppercase font-[Montserrat] m-4 float-left '>Wrafix</a>
-          <button><a href='t.me/Wrafix'>Telegram Wrafix</a></button>
-          <button type="button" class="float-right py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Referanslar</button>
-          <button type="button" class="float-right align-items  py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Projeler</button>
            
+           <div className=' float-right m-2'> 
+          
+          
+          <button type="button" class="float-right py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Projeler</button>
+          <button type="button" class="float-right align-items  py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Referanslar</button>
+           
+         
+          
+           </div>
        
        </ul>
     </div>

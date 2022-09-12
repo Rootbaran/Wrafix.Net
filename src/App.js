8b611840d4afc40b35bcae45040246b760b2f08e
baransoft <softbaran@yandex.com>
2022-09-13 00:47:38 +0300

    Demo ~V0.5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{ useState, useEffect } from 'react'
 import Header from './Components/Header'
 import Hero from './Components/Hero'
 import Discover from './Components/Discover'
+import Footer from './Components/Footer'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <Discover />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+/*class Footer extends React.Component { */
+
+function Footer() {
+    return (
+
+
+
+        <footer class=" text-center lg:text-left">
+        <div class=" text-center p-4 float-right m-2 ">
+          © 2022 Copyright:
+          <a class="text-gray-800" href="GitHub.Com/Rootbaran"> Wrafix.Net</a>
+        </div>
+      
+
+      </footer>
+         )
+
+     }
+
+
+export default Footer
